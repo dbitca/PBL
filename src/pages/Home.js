ad56715fac06094cbcdb1fd8b7d10c0323be125d
butnaruv <butnaru.valeria05@gmail.com>
2022-01-18 17:29:16 +0100

    homePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import Menu from "../components/Menu"
 import appMenu from "../components/appMenu"
 import Carousel from "../components/Carousel"
+import "../style/Home.css";
+
 const Home = () => {
     return(
     <div className="home-page">
         <Menu menu={appMenu}/>
         <div className="home-body">
-        <Carousel/>
+        <div className="welcome">Welcome to eDoc</div>
         </div>
+        <div ></div>
+        
     </div>)
 }
 
