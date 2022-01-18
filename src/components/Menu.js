eb401288ad60662eb3ec774bb3f3c87827a82973
Boris Butnaru <boris.butnaru@gmail.com>
2022-01-18 13:29:34 +0200

    add notifications to project

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,47 @@
 import "../style/Menu.css";
-import {
-    Link
-    } from 'react-router-dom'
-const Menu = ({menu}) => {
-
-return(<div class="main">
-<nav className="navigation">
-   <ul>
-       {
-           menu.map(e=>{
-               return (
-                <li key={e.url}><Link className="menu-url" to={e.url}>{e.name}</Link></li>
-
-               )
-           })
-       }
-      
-   </ul> 
-</nav>
-</div>);
-}
+import Notifications from "react-notifications-menu";
+import { Link } from "react-router-dom";
+const Menu = ({ menu }) => {
+  const DEFAULT_NOTIFICATION = {
+    image:
+      "https://cutshort-data.s3.amazonaws.com/cloudfront/public/companies/5809d1d8af3059ed5b346ed1/logo-1615367026425-logo-v6.png",
+    message: "Notification one.",
+    detailPage: "/events",
+    receivedTime: "12h ago",
+  };
+  return (
+    <div class="main">
+      <nav className="navigation">
+        <ul>
+          {menu.map((e) => {
+            return (
+              <li key={e.url}>
+                <Link className="menu-url" to={e.url}>
+                  {e.name}
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            {" "}
+            <Notifications
+              data={[]}
+              header={{
+                title: "Notifications",
+                option: {
+                  text: "View All",
+                  onClick: () => console.log("Clicked"),
+                },
+              }}
+              markAsRead={(data) => {
+                console.log(data);
+              }}
+            />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Menu;
--- src/components/appMenu.js
+++ src/components/appMenu.js
@@ -1,23 +1,24 @@
 const menu = [
-    {
-        url: "/home",
-        name: "Home",
-        type: "text"
-    },
-    {
-        url: "/about",
-        name: "About Us",
-        type: "text"
-    },
-    {
-        url: "/contact",
-        name: "Contact",
-        type: "text"
-    },
-    {
-        url: "/login",
-        name: "Log Out",
-        type: "text"
-    }
-]
+  {
+    url: "/home",
+    name: "Home",
+    type: "text",
+  },
+  {
+    url: "/categories",
+    name: "Categories",
+    type: "text",
+  },
+  {
+    url: "/about",
+    name: "About Us",
+    type: "text",
+  },
+
+  {
+    url: "/login",
+    name: "Log Out",
+    type: "text",
+  },
+];
 export default menu;
